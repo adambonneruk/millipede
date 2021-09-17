fed4a341a7e115f7c0bab2e6cb53e9e71660567a
adambonneruk <adam@bonner.uk>
2021-09-17 23:12:58 +0100

    added comments, search and replace now a function, updated readme

--- millipede.py
+++ millipede.py
@@ -1,25 +1,42 @@
+"""millipede is used for multiple consecutive RegEx search and replace opperations on a single text file"""
 import re
 
+def multi_search_and_replace(rows, rules):
+    results = []
+    row_count = len(rows) # count of the row array (e.g. lines in the .txt file)
+    rule_count = len(rules) # count of RegEx search and replace rules
+
+    for i in range(0,row_count):
+        results.append(rows[i])
+        for j in range(0,rule_count):
+            results[i] = re.sub(rules[j][0],rules[j][1],results[i])
+            """ rule[j] is the current search and replace rule
+                rule[j][0] is the zeroth column of the rule array, the "search"
+                rule[j][1] is the first column of the rule array, the "replace" """
+
+    return results
+
 def main():
-    rule = [
-        [r"[aeiou]",""],
-        [r"\.","_"],
-        [r" ",""],
+    # setup list of RegEx search and replace rules in a 2D array structure
+    regex_sar_rules = [
+        [r"[aeiou]", ""], # Remove Vowels
+        [r"\.", "_"], # Replace PEriod/Full Stop with Underscore
+        [r" ", ""] # Remove Spaces
     ]
 
-    row = []
+    # read the .txt file in as a 1D array where each line of the file is a new element
+    text_rows = []
     file = open(r'test/foobar.txt', "r")
     for line in file:
-        row.append(line.strip('\n'))
+        text_rows.append(line.strip('\n'))
     file.close()
 
-    rows = len(row)
-    rules = len(rule)
+    # perform the multiple search and replace rules for each line in the input file
+    results = multi_search_and_replace(text_rows,regex_sar_rules)
 
-    for i in range(0,rows):
-        for j in range(0,rules):
-            row[i] = re.sub(rule[j][0],rule[j][1],row[i])
-        print(row[i].lower())
+    # ...and print the output
+    for result in results:
+        print(result)
 
 if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -1,3 +1,29 @@
 # millipede
 
-apply multiple regex search and replace rules to a single document
+Apply multiple regex "Search and Replace" (SaR) rules to a single document. Using consecutive SaR rules, lines can be transformed using several simple steps each building on top of one another. I think this keeps things simple whilst aiding debugging. Using python we can even comment each SaR rule for future reference.
+
+## Example
+
+Using three rules we can (1) remove vowels, (2) replace full stops, and (3) remove spaces.
+
+### Replacement Rules
+
+```python
+regex_sar_rules = [
+    [r"[aeiou]", ""], # Remove Vowels
+    [r"\.", "_"], # Replace PEriod/Full Stop with Underscore
+    [r" ", ""] # Remove Spaces
+]
+```
+
+### Results Table (```test/foobar.txt```)
+
+| Before | After |
+| ------ | ----- |
+| Forename | Frnm |
+| Surname | Srnm |
+| Bank Details.Account Number | BnkDtls_AccntNmbr |
+| Bank Details.Sort Code | BnkDtls_SrtCd |
+| Address.Street | Addrss_Strt |
+| Address.Post Code | Addrss_PstCd |
+| Address.Country | Addrss_Cntry |
